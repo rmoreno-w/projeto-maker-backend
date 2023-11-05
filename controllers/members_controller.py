0018def736d5dd24ae6bd9a177c7adfb1461e56c
Rodrigo Moreno <rmoreno.w@gmail.com>
2023-11-05 04:21:54 -0300

    Controller buscar na DB ao inves de vetor fake

--- controllers/members_controller.py
+++ controllers/members_controller.py
@@ -3,29 +3,11 @@ from models.member import MakerMember
 
 router = APIRouter()
 
-banco_de_dados = [{
-        "name": "Claudiomar",
-        "cpf": "017660886888",
-        "email": "claudio.mar@unif.ei",
-        "address": {
-            "street": "Rua Prefeito Leão Jhonson",
-            "cep": "37800188",
-            "number": "522",
-            "district": "BPS",
-            "city": "Itajubá",
-            "state": "MG",
-            "complement": ""
-        },
-        "activation_state": True,
-        "password": "1234mil"
-    }
-]
-
 @router.post("/")
-def create_member(member_data: MakerMember):
-    banco_de_dados.append(member_data)
+async def create_member(member_data: MakerMember):
+    await member_data.save()
     return member_data
 
 @router.get("/")
-def get_members():
-    return { "users": banco_de_dados }
+async def get_members():
+    return await MakerMember.objects.all()
