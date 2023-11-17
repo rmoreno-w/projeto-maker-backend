a23fc06d87f05c03d1d202742ef903548d3f688c
Rodrigo Moreno <rmoreno.w@gmail.com>
2023-11-17 03:47:33 -0300

    Permissao de get materiais - membros

--- controllers/materials_controller.py
+++ controllers/materials_controller.py
@@ -11,10 +11,10 @@ router = APIRouter()
 @router.post("/")
 async def create_material(material_data: Material, logged_in_user: User = Depends(get_user_with_role(['admin']))):
     await material_data.save()
-    return {"details": "Material criado com sucesso"}
+    return {"details": "Material criado com sucesso", "id" : {material_data.id}}
 
 @router.get("/")
-async def get_materials(logged_in_user: User = Depends(get_user_with_role(['admin']))):
+async def get_materials(logged_in_user: User = Depends(get_user_with_role(['admin', 'member']))):
     return await Material.objects.all()
 
 @router.patch("/{material_id}")
