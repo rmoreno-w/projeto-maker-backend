bd042f537d04896c7931db77c5f6bb5195e7463d
Rodrigo Moreno <rmoreno.w@gmail.com>
2023-11-17 15:54:15 -0300

    Rota de alterar Serviço

--- controllers/services_controller.py
+++ controllers/services_controller.py
@@ -4,6 +4,7 @@ import ormar
 from fastapi import APIRouter, Depends, Response
 
 from controllers.depends.user import get_user_with_role
+from models.requests.service_update_data import ServiceUpdateData
 from models.responses.services_listing_response import ServiceInResponse
 from models.service import Service
 from models.user import User
@@ -18,3 +19,23 @@ async def create_service(service_data: Service, logged_in_user: User = Depends(g
 @router.get("/", response_model=List[ServiceInResponse])
 async def get_services(logged_in_user: User = Depends(get_user_with_role([]))):
     return await Service.objects.all()
+
+
+@router.patch("/{service_id}")
+async def update_service(service_id: int, response: Response, updated_fields: ServiceUpdateData, logged_in_user: User = Depends(get_user_with_role(['admin', 'member']))):
+    # print(logged_in_user)
+    try:
+        existing_service = await Service.objects.get(id=service_id)
+
+        fields_to_update = updated_fields.dict(exclude_unset=True) #Pega as propriedades que vieram da requisição, não inclui as não presentes
+
+        if len(fields_to_update) > 0:
+            await existing_service.update(**fields_to_update)
+
+        edited_fields = str(list(fields_to_update.keys()))
+        return {"message": f'{edited_fields} editado(s) com sucesso'}
+
+    except ormar.exceptions.NoMatch:
+        response_status_code = 404
+        response.status_code = response_status_code
+        return {"message" : f"Serviço de id {service_id} não encontrado"}
---
+++ models/requests/service_update_data.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class ServiceUpdateData(BaseModel):
+    name: Optional[str] = None
+    description:  Optional[str] = None
+    base_price: Optional[int] = None
